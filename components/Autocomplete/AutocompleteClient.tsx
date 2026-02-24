10d7223a9aba42d29afb5aa42204bfb050558b6d
Paul Meyer <okovessimon@yahoo.de>
2026-02-24 12:20:41 +0100

    refactor: switch from input to contentEditable

--- components/Autocomplete/AutocompleteClient.tsx
+++ components/Autocomplete/AutocompleteClient.tsx
@@ -5,6 +5,10 @@ import { pipeline, type TextGenerationPipeline } from '@huggingface/transformers
 
 export default function AutocompleteClient() {
   const generatorRef = useRef<TextGenerationPipeline | null>(null)
+
+  const contentEditableRef = useRef(null);
+
+  // load model once on component mount
   useEffect(() => {
     loadModel()
   }, [])
@@ -19,11 +23,23 @@ export default function AutocompleteClient() {
     console.log('model loaded')
   }
 
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.innerText
+    console.log('current value:', newValue)
+  }
+
   return (
-    <input
-      type="text"
-      className="w-full p-3 border rounded-lg"
-      placeholder="Tippe etwas..."
-    />
+    <div
+      className="p-3 border rounded-lg cursor-text text-left w-full h-[200px] mx-auto overflow-auto"
+    >
+      <span
+        ref={contentEditableRef}
+        className="outline-none"
+        contentEditable={true}
+        onInput={handleInput}
+      >
+        {/* {userText} */}
+      </span>
+    </div>
   )
 }
